ecde66420e26f941ad7c069881d7740ebf7e5283
Kamil <kamillopl@gmail.com>
2016-08-17 21:09:55 +0200

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static int fibonacci(int number) {
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+        return fibonacci(number-1)+ fibonacci(number-2);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter number upto which Fibonacci series to print: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println("Fibonacci series upto " + number + " numbers : ");
+        for (int i = 1; i<=number; i++ ){
+            System.out.print(fibonacci(i)+" ");
+        }
     }
 }
